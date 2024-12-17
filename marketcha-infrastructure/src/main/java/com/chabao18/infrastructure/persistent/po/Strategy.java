80752750204670b84e7388e69849cd669082342d
Zhixiong Lu <zhixiong.lu@qq.com>
2024-12-17 21:20:01 +0800

    :wrench: modify sql tables & test

---
+++ marketcha-infrastructure/src/main/java/com/chabao18/infrastructure/persistent/po/Strategy.java
@@ -0,0 +1,14 @@
+package com.chabao18.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Strategy {
+    private String id;
+    private String strategyId;
+    private String strategyDesc;
+    private Date createTime;
+    private Date updateTime;
+}
